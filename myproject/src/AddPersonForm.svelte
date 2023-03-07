da43f7952ad07669f79f68e227d8e01792c03441
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-07 07:00:06 -0800

    forms part 2 completed

--- myproject/src/AddPersonForm.svelte
+++ myproject/src/AddPersonForm.svelte
@@ -13,12 +13,18 @@
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name}>
-    <input type="text" placeholder="belt color" bind:value={beltColor}>
     <input type="number" placeholder="age" bind:value={age}>
     <label>Skills:</label>
     <input type="checkbox" bind:group={skills} value="fighting">fighting<br>
     <input type="checkbox" bind:group={skills} value="sneaking">sneaking<br>
     <input type="checkbox" bind:group={skills} value="running">running<br>
+    <label>Belt Color:</label>
+    <select bind:value={beltColor}>
+        <option value="black">Black</option>
+        <option value="orange">Orange</option>
+        <option value="brown">Brown</option>
+        <option value="white">White</option>
+    </select>
     <button>Add Person</button>
 </form>
 
